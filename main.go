e6ff194d362c518eaa20d831b22f949136ca94a2
novaari2 <novaari20@gmail.com>
2024-08-20 19:24:43 +0700

    delete koneksi

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 )
 
 func main(){
-	dsn := "root:root@tcp(127.0.0.1:3307)/campaign-project?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := ":@tcp()/?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
